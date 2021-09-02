2d44e3b8afad171db9e6f5814a0c54b50def28ed
raineardiente <82812944+raineardiente@users.noreply.github.com>
2021-09-02 17:37:38 +0800

    intial backend

--- signupfront/signupbackend/routes/routes.js
+++ signupfront/signupbackend/routes/routes.js
@@ -1,9 +1,22 @@
 const { request, response } = require('express');
 const express = require('express');
 const router = express.Router();
+const signUpTemplateCopy = require('../models/SignUpModels')
 
 router.post('/signup', (request, response) => {
-    response.send('send')
+   const signedUpUser = new signUpTemplateCopy({
+       fullName:request.body.fullName,
+       username:request.body.username,
+       email:request.body.email,
+       password:request.body.password
+   })
+   signedUpUser.save()
+   .then(data => {
+       response.json(data)
+   })
+   catch(error => {
+       response.json(error)
+   })
 })
 
 module.exports = router
