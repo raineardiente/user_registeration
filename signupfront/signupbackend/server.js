ab933912078b99efd469f11e0c6762774e28e517
raineardiente <82812944+raineardiente@users.noreply.github.com>
2021-09-02 18:19:55 +0800

    initial backend

--- signupfront/signupbackend/server.js
+++ signupfront/signupbackend/server.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
+
+dotenv.config()
+mongoose.connect(process.env.DATABASE_ACCESS, () => console.log('Database connected'))
 
 app.listen(4000, () => console.log("server is up and running"));
