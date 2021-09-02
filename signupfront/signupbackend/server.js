779cd8e108cded9aec3d1af624683c36a3e5858b
raineardiente <82812944+raineardiente@users.noreply.github.com>
2021-09-02 18:27:00 +0800

    initial backend

--- signupfront/signupbackend/server.js
+++ signupfront/signupbackend/server.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const routeUrls = require('./routes/routes');
+
 
 
 dotenv.config()
 mongoose.connect(process.env.DATABASE_ACCESS, () => console.log('Database connected'))
 
+app.use('/app', routeUrls)
 app.listen(4000, () => console.log("server is up and running"));
